1d92a0e58c391628d5400dfac10ecac1eda55857
stopusingpastebin <94611088+stopusingpastebin@users.noreply.github.com>
2022-02-12 22:17:13 +0300

    Update bruh.lua

--- bruh.lua
+++ bruh.lua
@@ -69,7 +69,7 @@ local Aiming = getgenv().Aiming
 -- // Create circle
 local circle = Drawingnew("Circle")
 circle.Transparency = 1
-circle.Thickness = 2
+circle.Thickness = 0.5
 circle.Color = Aiming.FOVColour
 circle.Filled = false
 Aiming.FOVCircle = circle
